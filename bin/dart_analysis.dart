a02bc12c9693c8c11528bcdfe12117793b97839a
paurakhsharma <paurakh011@gmail.com>
2025-07-17 10:57:28 +0545

    feat: add -v tag

--- bin/dart_analysis.dart
+++ bin/dart_analysis.dart
@@ -3,8 +3,28 @@
 import 'dart:io';
 import 'package:dart_analysis/analysis/unused_file_finder.dart';
 import 'package:interact_cli/interact_cli.dart';
+import 'package:path/path.dart' as p;
 
 void main(List<String> arguments) async {
+  // Version flag support (check anywhere in arguments)
+  if (arguments.any((arg) => arg == '-v' || arg == '--version')) {
+    final pubspecFile = File(
+      p.join(p.dirname(Platform.script.toFilePath()), '..', 'pubspec.yaml'),
+    );
+    if (pubspecFile.existsSync()) {
+      final pubspecContent = pubspecFile.readAsStringSync();
+      final versionMatch = RegExp(
+        r'^version:\s*(\S+)',
+        multiLine: true,
+      ).firstMatch(pubspecContent);
+      final version = versionMatch != null ? versionMatch.group(1) : 'unknown';
+      print('dart_analysis version $version');
+    } else {
+      print('dart_analysis version unknown');
+    }
+    exit(0);
+  }
+
   if (arguments.isEmpty) {
     print('Usage: dart run bin/dart_analysis.dart <path_to_project>');
     exit(1);
